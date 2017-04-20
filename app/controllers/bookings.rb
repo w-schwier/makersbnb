89b958bdf2727474a8c870575a2eb4a8eff26411
Will Schwier <w.schwier@hotmail.co.uk>
2017-04-20 18:04:08 +0100

    Refactoring

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -1,21 +1,16 @@
 class FourM < Sinatra::Base
 
   post '/bookings' do
-    # request = params[:booking]
     request = Request.first(id: params[:booking])
     @booking = request.confirm_booking
     all_request = Request.all(requested_date: request.requested_date)
-    #  require 'pry'; binding.pry
     request.destroy
     all_request.destroy
-    # if condition
-    #
-    # end
     redirect('/requests/confirmations')
   end
 
   get '/requests/confirmations' do
-    @bookings = Booking.all
+    @bookings = Booking.all(user_id: current_user.id)
     erb :'requests/confirmations'
   end
 
